c36984d981f58e3db6b355b092522b2d41b8f61e
Dissa2003 <164222210+Dissa2003@users.noreply.github.com>
2025-06-26 16:47:45 +0530

    do some changes

--- add_faces.py
+++ add_faces.py
@@ -4,6 +4,7 @@ video=cv2.VideoCapture(0)
 facedetect=cv2.CascadeClassifier('data/face.xml')
 
 
+faces_data=[]
 
 
 while(True):
@@ -11,9 +12,15 @@ while(True):
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces=facedetect.detectMultiScale(gray,1.3,5)
     for (x,y,w,h) in faces:
+        crop_img=frame[y:y+h,x:x+w,]
+        resized_img=cv2.resize(crop_img,(50,50))
+        if len(faces_data)<=100 and i%10==0:
+            faces_data.append(resized_img)
+        i=i+1 
+        
         cv2.rectangle(frame,(x,y),(x+w,y+h),(50,50,255),1)
     cv2.imshow('video', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if len(faces_data)==100 or cv2.waitKey(1)== ord('q'):
         break
 
 
