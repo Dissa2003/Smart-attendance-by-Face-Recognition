d6e2e683ed00ca0121e4bb109ed214f1d45e9f04
Dissa2003 <164222210+Dissa2003@users.noreply.github.com>
2025-06-28 15:40:11 +0530

    add data to pickle files

--- add_faces.py
+++ add_faces.py
@@ -1,4 +1,8 @@
 import cv2
+import pickle
+import numpy as np
+import os
+
 
 video=cv2.VideoCapture(0)
 facedetect=cv2.CascadeClassifier('data/face.xml')
@@ -6,6 +10,9 @@ facedetect=cv2.CascadeClassifier('data/face.xml')
 
 faces_data=[]
 
+i=0
+
+name=input("Enter the name of the person: ")
 
 while(True):
     ret,frame=video.read()
@@ -17,8 +24,11 @@ while(True):
         if len(faces_data)<=100 and i%10==0:
             faces_data.append(resized_img)
         i=i+1 
-        
+
+
+        cv2.putText(frame, str(len(faces_data)), (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (50,50,255), 1)
         cv2.rectangle(frame,(x,y),(x+w,y+h),(50,50,255),1)
+
     cv2.imshow('video', frame)
     if len(faces_data)==100 or cv2.waitKey(1)== ord('q'):
         break
@@ -27,3 +37,29 @@ while(True):
 
 video.release()
 cv2.destroyAllWindows()
+
+
+face_data=np.asarray(faces_data)
+face_data.reshape(100,-1)
+
+if 'names.pkl' not in os.listdir('data'):
+    names=[name]*100
+    with open('data/names.pkl','wb') as f:
+        pickle.dump(names,f)
+else:
+    with open('data/names.pkl','rb') as f:
+        names=pickle.load(f)
+    names=names+[name]*100
+    with open('data/names.pkl','wb') as f:
+        pickle.dump(names,f)
+
+
+if 'faces_data.pkl' not in os.listdir('data/'):
+    with open('data/faces_data.pkl', 'wb') as f:
+        pickle.dump(faces_data, f)
+else:
+    with open('data/faces_data.pkl', 'rb') as f:
+        faces=pickle.load(f)
+    faces=np.append(faces, faces_data, axis=0)
+    with open('data/faces_data.pkl', 'wb') as f:
+        pickle.dump(faces, f)        
